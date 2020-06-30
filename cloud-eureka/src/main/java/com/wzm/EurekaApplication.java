98102334254b65978efb85cba87ec0319d0a3072
wuzhiming <1054231937@qq.com>
2020-06-30 13:38:57 +0800

    注册中心

--- cloud-eureka/src/main/java/com/wzm/EurekaApplication.java
+++ cloud-eureka/src/main/java/com/wzm/EurekaApplication.java
@@ -2,12 +2,12 @@ package com.wzm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class SystemApplication {
+@EnableEurekaServer
+public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SystemApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
--- cloud-system/src/main/java/com/wzm/controller/TestController.java
+++ cloud-system/src/main/java/com/wzm/controller/TestController.java
@@ -3,8 +3,8 @@ package com.wzm.controller;
 import com.wzm.entity.ResultVO;
 import com.wzm.entity.Test;
 import com.wzm.service.TestService;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -14,15 +14,17 @@ import javax.annotation.Resource;
 public class TestController {
     @Resource
     private TestService testService;
+    @Value("${server.port}")
+    private String serverPort;
 
     @GetMapping("/system/queryTestById/{id}")
     public ResultVO<Test> getTestById(@PathVariable("id") Long id){
         log.info("查询成功");
         Test test = testService.queryTestById(id);
         if(test != null){
-            return new ResultVO<>(200,"查询成功", test);
+            return new ResultVO<>(200,"查询成功,serverPort: "+serverPort, test);
         }else {
-            return new ResultVO<>(500,"查询失败", null);
+            return new ResultVO<>(500,"查询失败,serverPort: "+serverPort, null);
         }
     }
 
@@ -31,9 +33,9 @@ public class TestController {
         log.info("插入数据库");
         int result = testService.addTest(test);
         if(result > 0){
-            return new ResultVO<>(200,"插入数据库成功", test);
+            return new ResultVO<>(200,"插入数据库成功,serverPort: "+serverPort, test);
         }else {
-            return new ResultVO<>(500,"插入数据库失败", null);
+            return new ResultVO<>(500,"插入数据库失败serverPort: "+serverPort, null);
         }
     }
 
